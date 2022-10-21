3c4c2ba08dd9d38b497532dad36c9d6b537b5642
n.thnptp <noppadol196@gmail.com>
2022-10-21 12:16:46 +0700

    update

--- Lab13 Methods/07.py
+++ Lab13 Methods/07.py
@@ -1,12 +1,32 @@
-# slice between first string and last string before number
-text = "AB12XY23"
-ext = []
-
-j = 0
-for i in range(len(text)):
-    test = text[i]
-    if text[i].isdigit():
-        ext.append(text[j:i])
-        j = i
+def extract_string(text):
+    if text == "":
+        return []
+    flag = "alpha"                  # check for the FIRST iteration if it is alphabet or digits
+    if text[0].isdigit():           # then set the flag
+        flag = "digit"
         
-print(ext)
+    ext = ""
+    j = 0
+    for i in range(len(text)):
+        ext += text[i]              # add current iteration to new variable "ext"
+        if i+1 < len(text):         # check if the next iteration is digit or alphabet
+            if text[i+1].isdigit() and flag == "alpha":
+                ext += "_"
+                flag = "digit"
+            if text[i+1].isdigit() == False and flag == "digit":
+                ext += "_"
+                flag = "alpha"
+            
+    ext = ext.split("_")           # split the extracted text
+    
+    for j in range(len(ext)):      # turn string to int if it is a digit
+        if ext[j].isdigit():
+            ext[j] = int(ext[j])
+            
+    return ext                     # return result
+
+print( extract_string("G2X3t4") )
+print( extract_string("AB12XY23") )
+print( extract_string("Number 4, Privet Drive") )
+print( extract_string("1 2 3 4 5 I love you.") )
+print( extract_string(""))
